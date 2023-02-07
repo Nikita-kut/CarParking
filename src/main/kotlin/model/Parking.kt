6b046b6003e1df6c4f8e0bebd6c16151f9e2d049
Kutcyi Nikita <kutcyi.nikita@gmail.com>
2023-02-07 14:17:36 +0300

    Kotlin #1. Работа со структурой данных

---
+++ src/main/kotlin/model/Parking.kt
@@ -0,0 +1,10 @@
+package model
+
+data class Parking(
+    val parkingSize: Int,
+    val parking: MutableList<Car>,
+) {
+    fun getCarsOnParking(): List<Car> = parking.filter { it.status == CarStatus.ON_PARKING }
+
+    fun getEmptyParkingPlace(): Int = parkingSize - getCarsOnParking().size
+}
